866e9359492a03b174633ce4329edf90b503dd21
madhuri7patel <madhuri7patel@gmail.com>
2022-09-11 13:24:58 +0530

    added data

--- strawberrynetProject-main/src/configs/db.js
+++ strawberrynetProject-main/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://aakash:aakash123@cluster0.hurs4.mongodb.net/strawberrynet?retryWrites=true&w=majority"
+    "mongodb+srv://strawberrynet:<password>@cluster0.2mjutaz.mongodb.net/test?retryWrites=true&w=majority"
   );
 };
 //mongodb+srv://aakash:aakash123@cluster0.hurs4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
